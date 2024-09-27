2ebfc8fd30cadb3ea3c7d85f6ea693b8b673b785
asma benzerrouk <asma.benzerrouk@gmail.com>
2024-09-27 14:06:46 +0200

    correction déclaration this.localStrage dans la class

--- Billed-app-FR-Front/src/containers/NewBill.js
+++ Billed-app-FR-Front/src/containers/NewBill.js
@@ -6,6 +6,7 @@ export default class NewBill {
 		this.document = document;
 		this.onNavigate = onNavigate;
 		this.store = store;
+		this.localStorage = localStorage;
 		const formNewBill = this.document.querySelector(
 			`form[data-testid="form-new-bill"]`
 		);
