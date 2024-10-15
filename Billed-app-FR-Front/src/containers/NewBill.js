38d8fe3db211fd313250e388a4043b12b7fc7c51
asma benzerrouk <asma.benzerrouk@gmail.com>
2024-10-15 15:51:14 +0200

    ajout commentaire

--- Billed-app-FR-Front/src/containers/NewBill.js
+++ Billed-app-FR-Front/src/containers/NewBill.js
@@ -19,19 +19,18 @@ export default class NewBill {
 		new Logout({ document, localStorage, onNavigate });
 	}
 	handleChangeFile = (e) => {
-		e.preventDefault();
 		const file = this.document.querySelector(`input[data-testid="file"]`)
 			.files[0];
 		const filePath = e.target.value.split(/\\/g);
 		const fileName = filePath[filePath.length - 1];
 		const allowedExtensions = ["jpg", "jpeg", "png"];
 		const fileExtension = fileName.split(".").pop().toLowerCase();
-
+		const formData = new FormData();
+		const email = JSON.parse(localStorage.getItem("user")).email;
+		formData.append("file", file);
+		formData.append("email", email);
+		// fix bug format accepté jpeg, jpg png
 		if (allowedExtensions.includes(fileExtension)) {
-			const formData = new FormData();
-			const email = JSON.parse(localStorage.getItem("user")).email;
-			formData.append("file", file);
-			formData.append("email", email);
 			this.store
 				.bills()
 				.create({
